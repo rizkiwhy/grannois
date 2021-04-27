cbd9519058fb37c9419910248b3740c2c0f1691f
rizkiwhy <rizkiiiwahyudi@gmail.com>
2021-04-28 03:16:09 +0700

    add data activityseeder

--- database/seeders/ActivitySeeder.php
+++ database/seeders/ActivitySeeder.php
@@ -22,7 +22,15 @@ class ActivitySeeder extends Seeder
                 'school_year' => 2020,
                 'start_date' => '2021-04-27',
                 'end_date' => '2021-04-27',
-                'note' => 'Kelulusan 2020/2021'
+                'note' => 'Kelulusan 2020/2021',
+            ],
+            [
+                // 'id' => Str::uuid(),
+                'activity_type_id' => 1,
+                'school_year' => 2021,
+                'start_date' => '2022-04-27',
+                'end_date' => '2022-04-27',
+                'note' => 'Kelulusan 2021/2022',
             ],
         ];
 
