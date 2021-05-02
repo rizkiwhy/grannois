4465df14f9b1e4a673b3edd1622fa42706daa6ee
rizkiwhy <rizkiiiwahyudi@gmail.com>
2021-05-03 01:28:47 +0700

    add create update delete competencyofexpertise

---
+++ resources/views/pages/master/curriculum/competencyofexpertise/index.blade.php
@@ -0,0 +1,190 @@
+@extends($data['layout'])
+@section('title', $data['page'] . ' | ' . $data['app'])
+@section('content-header')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">{{ $data['page'] }}</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">{{ $data['page'] }}</a></li>
+                        <li class="breadcrumb-item active">{{ $data['subpage'] }}</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                </div>
+                <div class="col-sm-6">
+                    <button class="btn btn-primary btn-sm mr-2 float-sm-right" data-toggle="modal"
+                        data-target="#createModal" data-backdrop="static">
+                        <i class="fas fa-plus mr-2"></i>Tambah
+                    </button>
+                </div>
+                <div class="modal fade" id="createModal">
+                    <div class="modal-dialog modal-dialog-centered modal-md">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title">Tambah {{ $data['page'] }}</h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <form action="{{ route('role.store') }}" method="post" class="form-horizontal"
+                                    id="createForm">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-sm-7 form-group">
+                                            <label for="nama">Nama</label>
+                                            <div class="input-group">
+                                                <input type="text" name="name" class="form-control" id="name"
+                                                    placeholder="Masukkan Nama">
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-5 form-group">
+                                            <label for="status">Status</label>
+                                            <div class="input-group">
+                                                <select class="form-control select2" id="active" name="active"
+                                                    style="width: 100%">
+                                                    <option value="" disabled selected>Pilih Status</option>
+                                                    <option value="1">Aktif</option>
+                                                    <option value="2">Tidak Aktif</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                            </div>
+                            <div class="modal-footer justify-content-between">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Tutup</button>
+                                <button type="submit" class="btn btn-primary">Simpan</button>
+                            </div>
+                            </form>
+                        </div>
+                        <!-- /.modal-content -->
+                    </div>
+                    <!-- /.modal-dialog -->
+                </div>
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+    @endsection
+    @section('main-content')
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Data {{ $data['page'] }}</h3>
+                                <div class="card-tools">
+                                    <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                        <i class="fas fa-minus"></i>
+                                    </button>
+                                    <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                                        <i class="fas fa-times"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th class="text-center">#</th>
+                                            <th class="text-center">Nama</th>
+                                            <th class="text-center">Status</th>
+                                            <th class="text-center">Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @php
+                                            $i = 1;
+                                        @endphp
+                                        @foreach ($data['role'] as $item)
+                                            <tr>
+                                                <td class="text-center">{{ $item->id }}</td>
+                                                <td>{{ $item->name }}</td>
+                                                <td class="text-center">
+                                                    @if ($item->active === 1)
+                                                        <span class="badge badge-success">Aktif</span>
+                                                    @else
+                                                        <span class="badge badge-danger">Tidak Aktif</span>
+                                                    @endif
+                                                </td>
+                                                <td class="text-center">
+                                                    <a class="btn btn-warning btn-sm"
+                                                        href="{{ route('role.edit', ['role' => $item->id]) }}"
+                                                        data-bs-toggle="tooltip" title="Ubah"><i
+                                                            class="fas fa-edit"></i></a>
+                                                    <button class="btn btn-danger btn-sm delete" data-toggle="modal"
+                                                        onclick="deleteItem({{ $item }})"
+                                                        data-target="#deleteModal" data-backdrop="static"
+                                                        data-bs-toggle="tooltip" title="Hapus">
+                                                        <i class="fas fa-trash"></i>
+                                                    </button>
+
+                                                </td>
+                                            </tr>
+                                            @php
+                                                $i++;
+                                            @endphp
+                                        @endforeach
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th class="text-center">#</th>
+                                            <th class="text-center">Nama</th>
+                                            <th class="text-center">Status</th>
+                                            <th class="text-center">Aksi</th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+
+                            <!-- /.card-body -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal fade" id="deleteModal">
+                <div class="modal-dialog modal-dialog-centered modal-md">
+                    <div class="modal-content">
+                        <div class="modal-header">
+
+                            <h4 class="modal-title">Hapus {{ $data['page'] }}
+                            </h4>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+
+                            <form action="#" method="post" class="form-horizontal" id="deleteForm">
+                                @method('delete')
+                                @csrf
+                                Apakah anda yakin akan menghapus Role <span name="textName" id="textName"></span>?
+                        </div>
+                        <div class="modal-footer justify-content-between">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Tutup</button>
+                            <button type="submit" class="btn btn-danger">Hapus</button>
+                        </div>
+                        </form>
+                    </div>
+                    <!-- /.modal-content -->
+                </div>
+                <!-- /.modal-dialog -->
+            </div>
+        </section>
+        <script src="{{ asset('src/plugins/jquery/jquery.min.js') }}"></script>
+        <script type="text/javascript">
+            function deleteItem(arr) {
+                $('#textName').text(arr.name)
+                $('#deleteForm').attr('action', `role/${arr.id}`)
+            }
+
+        </script>
+    @endsection
