f005e02ccb4a55e677de7140089b0f215ef8bb5c
rizkiwhy <rizkiiiwahyudi@gmail.com>
2021-05-04 07:55:04 +0700

    fix footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; {{ date('Y') }} <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; {{ date('Y') }} <a href="https://www.smkn11bdg.sch.id">ICT SMKN 11 BDG</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.1.0-rc
+        <b>Version</b> 1.1.0-rc
     </div>
 </footer>
