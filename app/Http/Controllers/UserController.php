97c8a9f39170a1149869c9bc783797240a591acf
rizkiwhy <rizkiiiwahyudi@gmail.com>
2021-05-10 15:27:52 +0700

    check status kelulusan

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -51,8 +51,6 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request->all());
-
         // validasi data request
         $request->validate([
             'name' => 'required|unique:users',
@@ -142,8 +140,6 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        dd($request->all());
-
         // cari user yang akan diubah
         $user = User::find($id);
 
